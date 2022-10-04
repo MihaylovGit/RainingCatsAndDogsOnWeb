d2a3a0bcd548ef3970c3262267c57d5ff6083b33
Kostadin Mihaylov <kmihaylov@90gmail.com>
2022-10-04 22:32:19 +0300

    Added Index View

---
+++ Data/RainingCatsAndDogsOnWeb.Data/Seeding/CategoriesSeeder.cs
@@ -0,0 +1,21 @@
+﻿namespace RainingCatsAndDogsOnWeb.Data.Seeding
+{
+    using RainingCatsAndDogsOnWeb.Data.Models;
+    using System;
+    using System.Linq;
+    using System.Threading.Tasks;
+
+    public class CategoriesSeeder : ISeeder
+    {
+        public async Task SeedAsync(ApplicationDbContext dbContext, IServiceProvider serviceProvider)
+        {
+            if (!dbContext.Categories.Any())
+            {
+                await dbContext.Categories.AddAsync(new Category { Name = "Dog" });
+                await dbContext.Categories.AddAsync(new Category { Name = "Cat" });
+
+                await dbContext.SaveChangesAsync();
+            }
+        }
+    }
+}
